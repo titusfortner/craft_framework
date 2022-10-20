2aca62f3e6a58d86d5b39f4c39cd6955e09df30c
titusfortner <titusfortner@gmail.com>
2022-10-20 07:45:27 -0500

    synchronize with explicit wait

--- src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/BaseTest.java
+++ src/test/java/com/saucelabs/advancedselenium/saucedemo/tests/BaseTest.java
@@ -6,9 +6,7 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.api.extension.TestWatcher;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.time.Duration;
 import java.util.Optional;
 
 public class BaseTest {
@@ -20,9 +18,7 @@ public class BaseTest {
     @BeforeEach
     public void setUp() {
         WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.setImplicitWaitTimeout(Duration.ofSeconds(2));
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
     }
 
     public class MyTestWatcher implements TestWatcher {
