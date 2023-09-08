85dc5d9420c403c111eb033b897f0daa3cc0d6bc
titusfortner <titus.fortner@gmail.com>
2023-09-07 23:05:22 -0500

    demonstrate race conditions

--- src/test/java/com/titusfortner/craft_framework/tests/BaseTest.java
+++ src/test/java/com/titusfortner/craft_framework/tests/BaseTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.api.extension.TestWatcher;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.Optional;
 
@@ -16,7 +18,9 @@ public class BaseTest {
 
     @BeforeEach
     public void setup() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NONE);
+        driver = new ChromeDriver(options);
     }
 
     public class MyTestWatcher implements TestWatcher {
